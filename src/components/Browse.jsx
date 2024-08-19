fd6f2857f64ad9def249bf23358c0da5a6cecf88
Bhuwan Thapa <thapabhuwan15@gmail.com>
2024-08-19 23:59:26 +0545

    browse

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,7 +1,31 @@
 const Browse = () => {
   return (
-    <div>
-      <h1 className="text-5xl font-semibold">Browse through repository</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-[80%] flex justify-between items-center overflow-hidden">
+        <div className="w-[48%]">
+          <div className="mb-[20%]">
+            <h1 className="text-5xl font-semibold mb-[5%]">
+              Browse through repository
+            </h1>
+            <p>
+              Our advanced search algorithm helps you find the most relevant
+              GitHub repositories based on github’s username, including the
+              project description, languages, stars, and recent commits.
+            </p>
+          </div>
+          <div className="">
+            <h1 className="text-5xl font-semibold mb-[5%]">Master GitHub </h1>
+            <p>
+              Dive deep into repository analytics by understanding commit
+              patterns, contributor dynamics, and code evolution over time.
+            </p>
+            <p className="text-green-800 mb-[2%]">Start Your GitHub Journey</p>
+          </div>
+        </div>
+        <div className="w-[48%]">
+          <img src="/computer.jpg" alt="computer" className="rounded" />
+        </div>
+      </div>
     </div>
   );
 };
