217e298192a7be773066f37bae0827458882967a
Bhuwan Thapa <thapabhuwan15@gmail.com>
2024-08-23 11:05:02 +0545

    tracing beam

--- src/app/page.js
+++ src/app/page.js
@@ -10,16 +10,19 @@ import Community from "@/components/Community";
 import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
 import { BackgroundBeams } from "@/components/BackgroundBeam";
+import { TracingBeam } from "@/components/TracingBeam";
 
 export default function Home() {
   return (
     <div>
       <BackgroundBeams />
       <Hero />
-      <Start />
-      <ImgGallary className="mt-24" />
-      <Browse className="mt-24" />
-      <Community className="mt-24" />
+      <TracingBeam>
+        <Start />
+        <ImgGallary className="mt-24" />
+        <Browse className="mt-24" />
+        <Community className="mt-24" />
+      </TracingBeam>
       <Banner className="mt-28" />
       <Footer />
     </div>
